d831988c2498fb95fec9cc58d65077433da7b5c4
Chaos Pjeles <fqrious@gmail.com>
2025-02-28 07:14:52 +0100

    initial impl (#2)
    * initial impl
    
    * adding skeleton docs
    
    * write to stix2_objects/ for debug
    
    * add --combine #4 and --group_name args #3
    
    * clean up
    
    * adding wording fixes
    
    * Update parser.py
    
    * adding more parsers
    
    * split bundles and stix2_objects #5 and #7
    
    * fix incident.description #8
    
    * Update README.md
    
    * Update .gitignore
    
    * fix regression causing overwrite error #10
    
    * updating docs
    
    * make victim name lower case #12
    
    * use deterministic id for incident #14 and its relationship #15
    
    * change relationship_type and description to match #15
    
    * updating docs
    
    * updating docs
    
    * Update README.md
    
    * updating docs to match new spec
    
    * fix uuid for incident #14
    
    * make victim domain lowercase in identity.contact_information #12
    
    * fix relationship uuid #16
    
    * use x-mitre-tactic instead of attack-pattern #17
    
    * make changes for r2db
    
    * fix relationship uuid #16
    
    * fix error on tool creation #17
    
    * use precision.MILISECOND instead of default ANY #16
    
    * Update parser.py
    
    ---------
    
    Co-authored-by: David G <himynamesdave@users.noreply.github.com>
    Co-authored-by: Fadl <folawumi@dogesec.com>

---
+++ ransomware2stix/retriever.py
@@ -0,0 +1,63 @@
+from functools import lru_cache
+import logging
+import os
+from urllib.parse import urljoin
+import requests
+
+
+DEFAULT_OBJECT_URLS = [
+    "https://raw.githubusercontent.com/muchdogesec/stix4doge/refs/heads/main/objects/marking-definition/ransomware2stix.json",
+    "https://raw.githubusercontent.com/muchdogesec/stix4doge/refs/heads/main/objects/identity/ransomware2stix.json",
+]
+
+#####
+def get_groups():
+    r = requests.get('https://api.ransomware.live/v1/groups')
+    return {group['name']: group for group in r.json()}
+
+def get_victims():
+    resp = requests.get('https://data.ransomware.live/victims.json')
+    return resp.json()
+
+@lru_cache
+def get_default_objects():
+    return [requests.get(url).json() for url in DEFAULT_OBJECT_URLS]
+
+
+def _get_objects(endpoint, headers):
+    data = []
+    page = 1
+    while True:
+        resp = requests.get(endpoint, params=dict(page=page, page_size=1000), headers=headers)
+        if resp.status_code != 200:
+            break
+        d = resp.json()
+        if len(d['objects']) == 0:
+            break
+        data.extend(d['objects'])
+        page+=1
+        if d['page_results_count'] < d['page_size']:
+            break
+    return data
+
+def get_attack_objects(attack_ids):
+    if not attack_ids:
+        return []
+    logging.debug(f"retrieving attack objects: {attack_ids}")
+    endpoint = urljoin(os.environ['CTIBUTLER_BASE_URL'] + '/', f"v1/attack-enterprise/objects/?attack_id="+','.join(attack_ids))
+
+    headers = {}
+    if api_key := os.environ.get('CTIBUTLER_API_KEY'):
+        headers['API-KEY'] = api_key
+
+    return _get_objects(endpoint, headers)
+
+@lru_cache
+def get_location_objects():
+    logging.info(f"retrieving location objects")
+    endpoint = urljoin(os.environ['CTIBUTLER_BASE_URL'] + '/', f"v1/location/objects/?location_type=country")
+    headers = {}
+    if api_key := os.environ.get('CTIBUTLER_API_KEY'):
+        headers['API-KEY'] = api_key
+
+    return _get_objects(endpoint, headers)
